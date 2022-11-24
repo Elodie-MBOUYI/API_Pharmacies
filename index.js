d906ceee7ff6ed0837258fa8445779dc9a93c364
Elod-cpu <oulendoeloise@gmail.com>
2022-11-24 04:31:43 +0100

    api

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ var mysql = require("mysql")
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Bienvenue sur l\'API de AFRIJET des Pharmacies de POG')
+    res.send('Bienvenue sur l\'API des Pharmacies de POG')
 })
 
+app.listen(process.env.PORT ||3500)
 
 app.use(express.json())
 app.use(cors())
@@ -27,4 +28,50 @@ con.connect((err)=>{
         console.log('connexion établie');
     }
 })
+
+// Lister les produits enregistrés dans la base de données de la pharmacie choisie;
+app.get('/api/produits', (req, res)=>{
+    
+    con.query('SELECT * FROM produits',(err,result)=>{
+        if(err) res.status(500).send(err)
+        
+        res.status(200).json(result)
+    })
+})
+
+//Ajouter les produits pharmaceutiques dans la base de données;
+app.post('/api/produits/ajout', (req, res)=>{
+    const proprietes = req.body.proprietes;
+    const produits = req.body.produits;
+    const description = req.body.description;
+    const prix = req.body.prix;
+
+    
+    con.query('INSERT INTO produits VALUES(NULL,?,?,?,?,?)',[proprietes,produits,description,prix],(err,result)=>{
+        if(err)
+    {
+        console.log(err)
+    }else{
+        res.send('POSTED');
+    }
+    })
+})
+
+//Ajouter les noms de pharmacies de garde dans la base de données
+app.post('/api/pharmgard/ajout', (req, res)=>{
+    const nom_pharmgard = req.body.nom_pharmgard;
+    const telephone = req.body.telephone;
+    
+    
+    con.query('INSERT INTO marque VALUES(NULL,?,?)',[nom_pharmgard,telephone],(err,result)=>{
+        if(err)
+    {
+        console.log(err)
+    }else{
+        res.send('POSTED');
+    }
+    })
+})
+
+
 
